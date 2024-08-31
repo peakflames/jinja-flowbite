d573025680286f0dd6a4380e528aeb5fc7fcb6e1
Todd Schavey <tschavey@archer.com>
2024-08-30 23:04:50 -0400

    roll to v0.3.dev3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as rq:
 
 setup(
     name="jinja_flowbite",
-    version="0.3.dev2",
+    version="0.3.dev3",
     platforms="any",
     description="Flowbite-Based Jinja Components",
     long_description=readme_md,
