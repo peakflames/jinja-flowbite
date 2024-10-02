dcd4e59865a82d88b96b496e32506f2d118fcb2e
Todd Schavey <tschavey@archer.com>
2024-10-02 19:55:29 -0400

    roll to v0.3.dev7: update input_text to support pattern attribute

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as rq:
 
 setup(
     name="jinja_flowbite",
-    version="0.3.dev6",
+    version="0.3.dev7",
     platforms="any",
     description="Flowbite-Based Jinja Components",
     long_description=readme_md,
